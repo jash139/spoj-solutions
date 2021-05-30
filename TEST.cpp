02e5a0e15e7ae1c22dd7645c1755375d2cb7a330
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-31 00:15:21 +0530

    TEST

---
+++ TEST.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	while (true) {
+		int n;
+		cin>>n;
+		if (n == 42) {
+			break;
+		}
+		cout<<n<<"\n";
+	}
+	
+	return 0;
+}
